05bd0733f970eb274cc58d007b75bd9dc85022d5
Aldo Pernumian <aldo.pernumian@gmail.com>
2026-02-21 15:36:32 -0300

    fix: update design-time factory to use SQLEXPRESS_2022 instance

--- src/AiOdonto.Api/Data/AppDbContextFactory.cs
+++ src/AiOdonto.Api/Data/AppDbContextFactory.cs
@@ -8,7 +8,7 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     public AppDbContext CreateDbContext(string[] args)
     {
         var options = new DbContextOptionsBuilder<AppDbContext>()
-            .UseSqlServer("Server=localhost;Database=AiOdonto;Trusted_Connection=true;TrustServerCertificate=true")
+            .UseSqlServer("Server=localhost\\SQLEXPRESS_2022;Database=AiOdonto;Trusted_Connection=true;TrustServerCertificate=true")
             .Options;
         return new AppDbContext(options);
     }
