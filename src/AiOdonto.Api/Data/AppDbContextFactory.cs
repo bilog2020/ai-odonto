2dc4dc87ac86ab6e3800969927eb93ba28b23e06
Aldo Pernumian <aldo.pernumian@gmail.com>
2026-02-21 15:28:14 -0300

    feat: add initial EF Core migration and design-time factory

---
+++ src/AiOdonto.Api/Data/AppDbContextFactory.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AiOdonto.Api.Data;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlServer("Server=localhost;Database=AiOdonto;Trusted_Connection=true;TrustServerCertificate=true")
+            .Options;
+        return new AppDbContext(options);
+    }
+}
